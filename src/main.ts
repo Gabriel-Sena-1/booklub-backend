6c98a11cbaf27db0651a47a165508721a2d54523
Gabriel-Sena-1 <gabriel.sena@tarken.ag>
2025-12-28 20:22:39 -0400

    feat: add url prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
     .setTitle('Booklub API')
@@ -11,7 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth() // 🔒 caso use JWT futuramente
     .build();
-  
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document); // acessa em http://localhost:3000/docs
 
