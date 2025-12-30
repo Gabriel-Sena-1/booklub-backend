ac69191118d96198ac207944c140cc7a5d550168
Guilherme Fiani <gfiani10@gmail.com>
2025-12-29 23:54:51 -0400

    feat(checkouts): implementa validação de páginas lidas e create do checkout com data fixa

--- src/modules/checkouts/checkouts.module.ts
+++ src/modules/checkouts/checkouts.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Checkout } from './entities/checkout.entity';
 import { CheckoutsController } from './checkouts.controller';
 import { CheckoutsService } from './checkouts.service';
+import { UsersBooksModule } from '../users-books/users-books.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Checkout])],
+  imports: [
+    TypeOrmModule.forFeature([Checkout]),
+    UsersBooksModule,
+  ],
   controllers: [CheckoutsController],
   providers: [CheckoutsService],
   exports: [TypeOrmModule],
