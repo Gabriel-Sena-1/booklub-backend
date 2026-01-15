711376d3925fe706595e3d07cf17d47a6ee683f6
Guilherme Fiani <gfiani10@gmail.com>
2026-01-15 01:26:00 -0400

    fix(auth): fix access guarantee on Public() endpoints

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -6,15 +6,23 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private jwtService: JwtService) {}
+    constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (isPublic) {
+            return true;
+        }
+
         const request = context.switchToHttp().getRequest<Request>();
         const token = this.extractTokenFromHeader(request);
-        
         if (!token) {
             throw new UnauthorizedException("No token provided");
         }
