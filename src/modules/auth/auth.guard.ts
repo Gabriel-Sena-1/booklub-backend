3a0c2ea743cdc60e088412a5ddf456bbeabdc35b
Guilherme Fiani <gfiani10@gmail.com>
2026-01-15 00:47:16 -0400

    feat(auth): add guard for authentication using jwt

---
+++ src/modules/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private jwtService: JwtService) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest<Request>();
+        const token = this.extractTokenFromHeader(request);
+        
+        if (!token) {
+            throw new UnauthorizedException("No token provided");
+        }
+        try { 
+            const payload = await this.jwtService.verifyAsync(token);
+            request["user"] = payload; 
+        } catch {
+            throw new UnauthorizedException("Invalid token");
+        }
+        return true;
+    }
+
+    private extractTokenFromHeader(request: Request): string | null {
+        const [type, token] = request.headers.get("authorization")?.split(" ") ?? [];
+        return type === "Bearer" ? token : null;
+    }
+}
